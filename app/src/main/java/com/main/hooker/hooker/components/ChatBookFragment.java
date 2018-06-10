9030c8b3a8049b898923e6e8c58669f8212d1da5
DizyZeng <mhisf@vip.qq.com>
2018-06-10 15:48:56 +0800

    load bubble finished

--- app/src/main/java/com/main/hooker/hooker/components/ChatBookFragment.java
+++ app/src/main/java/com/main/hooker/hooker/components/ChatBookFragment.java
@@ -6,30 +6,48 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.main.hooker.hooker.R;
 import com.main.hooker.hooker.entity.Book;
+import com.main.hooker.hooker.entity.BookWrapper;
 import com.main.hooker.hooker.entity.Bubble;
+import com.main.hooker.hooker.model.BookModel;
+import com.main.hooker.hooker.utils.Tool;
+import com.main.hooker.hooker.utils.http.ApiFailException;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 
 public class ChatBookFragment extends Fragment {
 
     private List<Bubble> bubbles = new ArrayList<>();
+    private List<Bubble> bubbleCache = new ArrayList<>();
     private Book book;
+    private int mPage = 0;
+    private int mNowItem = -1;
+    private boolean mHasMore = true;
+    private BubbleAdapter mAdapter;
+    private RecyclerView mRecyclerView;
 
     public static ChatBookFragment newInstance(Book book) {
+        return newInstance(book, 1);
+    }
+
+    public static ChatBookFragment newInstance(Book book, int nowPage) {
         ChatBookFragment fragment = new ChatBookFragment();
         fragment.book = book;
+        fragment.mPage = nowPage - 1;
         return fragment;
     }
 
@@ -42,20 +60,83 @@ public class ChatBookFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        bubbles.add(new Bubble());
         Picasso.get().load(book.cover_img).into((ImageView) view.findViewById(R.id.cover));
         view.findViewById(R.id.title).setOnClickListener((v) -> getActivity().getSupportFragmentManager().popBackStack());
         View layout = view.findViewById(R.id.mainLayout);
-        RecyclerView recyclerView = view.findViewById(R.id.recycler);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        BubbleAdapter adapter = new BubbleAdapter(R.layout.item_bubble, bubbles);
-        adapter.openLoadAnimation(BaseQuickAdapter.SCALEIN);
-        layout.setOnClickListener(v -> adapter.addData(new Bubble()));
-        adapter.setOnItemClickListener((adapter1, view1, position) -> {
+        mRecyclerView = view.findViewById(R.id.recycler);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mAdapter = new BubbleAdapter(R.layout.item_bubble, bubbles);
+        mAdapter.openLoadAnimation(BaseQuickAdapter.SCALEIN);
+        layout.setOnClickListener(v -> getNewBubble());
+        mAdapter.setOnItemClickListener((adapter1, view1, position) -> {
             layout.performClick();
-            recyclerView.scrollToPosition(adapter.getItemCount() - 1);
         });
-        recyclerView.setAdapter(adapter);
+        mRecyclerView.setAdapter(mAdapter);
+        getNewBubble();
+    }
+
+    public void getNewBubble(){
+        if(!mHasMore){
+            Toast.makeText(getContext(), "There is no more", Toast.LENGTH_LONG).show();
+        }
+        mNowItem += 1;
+        if(bubbleCache.size() > mNowItem){
+            mAdapter.addData(bubbleCache.get(mNowItem));
+            mRecyclerView.scrollToPosition(mAdapter.getItemCount() - 1);
+        } else {
+            load(() -> {
+                if(bubbleCache.size() > mNowItem && mHasMore){
+                    mAdapter.addData(bubbleCache.get(mNowItem));
+                    mRecyclerView.scrollToPosition(mAdapter.getItemCount() - 1);
+                }
+            });
+        }
+    }
+
+    public void load(){
+        load(false, null);
+    }
+
+    public void load(Runnable callback){
+        load(false, callback);
+    }
+
+    public void load(boolean backward, Runnable callback){
+        if(!backward){
+            mPage += 1;
+        }else{
+            if(mPage<=0) return;
+            mPage -= 1;
+        }
+        new Thread(() -> {
+            try {
+                ArrayList<Bubble> list = BookModel.getBubbles(book.id, mPage);
+                getActivity().runOnUiThread(() -> {
+                    if(!backward) {
+                        if(list==null || list.size()==0){
+                            mHasMore = false;
+                            Toast.makeText(getContext(), "There is no more", Toast.LENGTH_LONG).show();
+                            return;
+                        }
+                        bubbleCache.addAll(list);
+                    }else {
+                        if(list==null || list.size()==0){
+                            mHasMore = false;
+                            Toast.makeText(getContext(), "There is no more", Toast.LENGTH_LONG).show();
+                            return;
+                        }
+                        bubbleCache.addAll(0, list);
+                        mNowItem += list.size();
+                    }
+                    if(callback != null){
+                        callback.run();
+                    }
+                });
+            } catch (ApiFailException e) {
+                Log.e("api", "load: "+e.getApiResult().msg);
+                //  Toast.makeText(getContext(), "Error:" + e.getApiResult().msg, Toast.LENGTH_LONG).show();
+            }
+        }).start();
     }
 
 
@@ -67,12 +148,12 @@ public class ChatBookFragment extends Fragment {
         @Override
         protected void convert(BaseViewHolder helper, Bubble item) {
             if (item.position == 0) {
-                helper.setText(R.id.character, item.character.name);
+                helper.setText(R.id.character, item.character == null ? "unknown" : item.character.name);
                 helper.setText(R.id.content, item.content);
                 helper.setGone(R.id.card, true);
                 helper.setGone(R.id.card2, false);
             } else {
-                helper.setText(R.id.character2, item.character.name);
+                helper.setText(R.id.character2, item.character == null ? "unknown" : item.character.name);
                 helper.setText(R.id.content2, item.content);
                 helper.setGone(R.id.card2, true);
                 helper.setGone(R.id.card, false);
