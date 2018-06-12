fafb782ffac61ccf4e24132d0da3531ccdfebf91
neoming <10165101145@stu.ecnu.edu.cn>
2018-06-12 13:19:39 +0800

    add book edit operation : book item(bubble) ,book character

---
+++ app/src/main/java/com/main/hooker/hooker/adapter/BookAdapter.java
@@ -0,0 +1,31 @@
+package com.main.hooker.hooker.adapter;
+
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.main.hooker.hooker.R;
+import com.main.hooker.hooker.entity.Book;
+import com.main.hooker.hooker.entity.User;
+
+import java.util.List;
+
+/**
+ * Created by ASUS on 2018/6/11.
+ */
+
+public class BookAdapter extends BaseQuickAdapter<Book, BaseViewHolder> {
+    public BookAdapter(int layoutResId, @Nullable List<Book> data) {
+        super(layoutResId, data);
+    }
+
+
+    @Override
+    protected void convert(BaseViewHolder helper, Book item) {
+        //helper.setText(,"");
+        Log.d(TAG, "convert: book = " + item.title);
+        helper.setText(R.id.search_book_title,item.title);
+        helper.setText(R.id.search_book_author,"xyy");
+    }
+}
--- app/src/main/java/com/main/hooker/hooker/views/SearchActivity.java
+++ app/src/main/java/com/main/hooker/hooker/views/SearchActivity.java
@@ -2,15 +2,90 @@ package com.main.hooker.hooker.views;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.main.hooker.hooker.R;
+import com.main.hooker.hooker.adapter.BookAdapter;
+import com.main.hooker.hooker.adapter.FollowAdapter;
+import com.main.hooker.hooker.components.LoginFragment;
+import com.main.hooker.hooker.components.ProfileDetailFragment;
+import com.main.hooker.hooker.entity.Book;
+import com.main.hooker.hooker.entity.User;
+import com.main.hooker.hooker.model.BookModel;
+import com.main.hooker.hooker.model.UserModel;
+import com.main.hooker.hooker.utils.http.ApiFailException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private  SearchView book_searchView=null;
+    private  List<Book> searched_books=new ArrayList<>();
+    private  BookAdapter bookAdapter=null;
+    private  RecyclerView recyclerView=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        View header = View.inflate(this,R.layout.header_add_book,null);
+        bookAdapter = new BookAdapter(R.layout.item_serach_book,searched_books);
+        bookAdapter.addHeaderView(header);
+
+        recyclerView = findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(bookAdapter);
+        book_searchView = header.findViewById(R.id.bookSearchView);
+        if(book_searchView == null){
+            Toast.makeText(this,"can not find bookSearchVie",Toast.LENGTH_SHORT).show();
+        }else {
+            book_searchView.setIconifiedByDefault(true);
+            book_searchView.setSubmitButtonEnabled(true);
+            book_searchView.setIconifiedByDefault(true);
+            book_searchView.setQueryHint("Find stories");
+            book_searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    new Thread(() -> {
+                        try {
+                            searched_books = BookModel.getSearchList(query,0);
+                            if(!searched_books.isEmpty()){
+                                runOnUiThread(()->{
+                                    bookAdapter.setNewData(searched_books);
+                                });
+                            }
+                        } catch (ApiFailException e) {
+                            e.printStackTrace();
+                        }
+                    }).start();
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    new Thread(() -> {
+                        try {
+                            searched_books = BookModel.getSearchList(newText,0);
+                            if(!searched_books.isEmpty()){
+                                runOnUiThread(()->{
+                                    bookAdapter.setNewData(searched_books);
+                                });
+                            }
+                        } catch (ApiFailException e) {
+                            e.printStackTrace();
+                        }
+                    }).start();
+                    return false;
+                }
+            });
+        }
     }
 
     @Override
